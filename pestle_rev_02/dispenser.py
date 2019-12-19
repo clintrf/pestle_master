e22d1b850149d6faa665720939dc9129a9708b03
Clint <clint.frandsen@gmail.com>
2019-12-18 22:24:06 -0700

    rev2

--- pestle_rev_02/dispenser.py
+++ pestle_rev_02/dispenser.py
@@ -8,17 +8,13 @@ from hx711 import HX711
 
 
 # Pins are in GPIO.BCM mode
-L293D_ENB_M1_PIN = 17
+L293D_ENB_M1_PIN = 22
 L293D_INPUT1_M1_PIN = 27
-L293D_INPUT2_M1_PIN = 22
+L293D_INPUT2_M1_PIN = 17
 
-L293D_ENB_M2_PIN = 18
-L293D_INPUT1_M2_PIN = 16
-L293D_INPUT2_M2_PIN = 12
-
-L293D_ENB_M3_PIN = 13
-L293D_INPUT1_M3_PIN = 13
-L293D_INPUT2_M3_PIN = 26
+L293D_ENB_M2_PIN = 5
+L293D_INPUT1_M2_PIN = 6
+L293D_INPUT2_M2_PIN = 13
 
 
 class DispenserInterface(object):
@@ -49,32 +45,30 @@ class TestDispenser(DispenserInterface):
 class Dispenser(DispenserInterface):
     def __init__(self):
         # Set the pins for the scale. Pins are in GPIO.BCM mode
-        self._hx = HX711(5, 6)
+        #self._hx = HX711(5, 6)
         # Set and initialize the pins for the 2 motors
-        #GPIO.setmode(GPIO.BCM)
-        GPIO.setup(L293D_ENB_PIN, GPIO.OUT)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(L293D_ENB_M1_PIN, GPIO.OUT)
+        GPIO.setup(L293D_ENB_M2_PIN, GPIO.OUT)
         
         GPIO.setup(L293D_INPUT1_M1_PIN, GPIO.OUT, initial=GPIO.HIGH)
         GPIO.setup(L293D_INPUT2_M1_PIN, GPIO.OUT, initial=GPIO.LOW)
 
         GPIO.setup(L293D_INPUT1_M2_PIN, GPIO.OUT, initial=GPIO.HIGH)
         GPIO.setup(L293D_INPUT2_M2_PIN, GPIO.OUT, initial=GPIO.LOW)
-        
-        GPIO.setup(L293D_INPUT1_M3_PIN, GPIO.OUT, initial=GPIO.HIGH)
-        GPIO.setup(L293D_INPUT2_M3_PIN, GPIO.OUT, initial=GPIO.LOW)
 
         # Set the read in format for first the Pi and then the Hx711 board (MSB/LSB most/least sig bit)
-        self._hx.set_reading_format("MSB", "MSB")
+        #self._hx.set_reading_format("MSB", "MSB")
 
         # HOW TO CALCULATE THE REFFERENCE UNIT
         # If 2000 grams is 184000 then 1 gram is 184000 / 2000 = 92.
         # hx.set_reference_unit(92)
         # hx.set_reference_unit(calibrateInGrams(self.weight))
-        self._hx.set_reference_unit(3038)
+        #self._hx.set_reference_unit(3038)
 
         # Reset and tare scale
-        self._hx.reset()
-        self._hx.tare()
+        #self._hx.reset()
+        #self._hx.tare()
         #while(1):
         #    print(self._hx.get_weight(5))
         #print('Tare done!')
@@ -89,24 +83,23 @@ class Dispenser(DispenserInterface):
         #print('Dispensing %0.2f grams from slot %d' % (amount, slot_idx))
         #print(amount + " "  + "\r\n")
 
-        self._hx.tare()
+        #self._hx.tare()
         if slot_idx == 0:  # if it's salt
             motor_pin = L293D_ENB_M1_PIN
         elif slot_idx == 1:  # if it's pepper
             motor_pin = L293D_ENB_M2_PIN
-        elif slot_idx == 2:
-            motor_pin = L293D_ENB_M3_PIN
         else:
             print("motor should not be going")
 
         motor = GPIO.PWM(motor_pin,50) # pwm frequency: 50hz
-        val = self._hx.get_weight(5)
+        #val = self._hx.get_weight(5)
         val = 0
         print(amount)
+        
         while val <= amount:
-            val = self._hx.get_weight(5)
+            #val = self._hx.get_weight(5)
             print(val)
-            motor.start(70)  # duty cycle: check to see if we need to increase the param
+            motor.start(100)  # duty cycle: check to see if we need to increase the param
             val= val+1
         motor.stop()
         
